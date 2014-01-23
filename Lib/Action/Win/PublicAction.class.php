1e9dfc6bb3a009b6056a36849ac97e4cd6c8c7a5
pang <dianshiweijin@126.com>
2014-01-23 19:10:52 +0800

    Signed-off-by: pang <dianshiweijin@126.com>

--- Lib/Action/Win/PublicAction.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('THINK_PATH') or exit();
-	class PublicAction extends Action{
-		public function verify(){
-			import('ORG.Util.Image');
-			Image::buildImageVerify();
-		}
-	}
-?>
--- install.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('THINK_PATH', './framework/');
-define('APP_NAME','Install');
-define('APP_PATH', './Install/');
-define('APP_DEBUG', true);
-include THINK_PATH."ThinkPHP.php";
-?>
