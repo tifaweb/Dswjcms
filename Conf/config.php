eb09494361256f1ef3277307108c879528e3795e
tifaweb <dianshiweijin@126.com>
2016-04-11 15:19:01 +0800

    dswjcms p2p1.4.3更新

--- Conf/config.php
+++ Conf/config.php
@@ -56,6 +56,6 @@ return array(
 	'DS_ENTERPRISE'			=>	'点石为金借贷系统',
 	'DS_EN_ENTERPRISE'		=>	'dswjjd',
 	'DS_TOP_POWERED'		=>	'Powered by Dswjcms!',
-	'DS_POWERED'			=>	'<p class="pull-left">Powered by <strong><a href="http://www.dswjcms.com" target="_blank">Dswjcms!</a></strong> <em>1.4.2</em><br/>&copy; 2003-2018 <a href="http://www.tifaweb.com" target="_blank">Tf Inc.</a></p>',
+	'DS_POWERED'			=>	'<p class="pull-left">Powered by <strong><a href="http://www.dswjcms.com" target="_blank">Dswjcms!</a></strong> <em>1.4.3</em><br/>&copy; 2003-2018 <a href="http://www.tifaweb.com" target="_blank">Tf Inc.</a></p>',
 );
 ?>
